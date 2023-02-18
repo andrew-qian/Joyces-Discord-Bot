207fdab94d059994b0f68bb535dce6dcde305d90
andrew-qian <andrewyukaiqian@gmail.com>
2023-02-18 13:12:37 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,12 @@ async def main(ctx, oldelements):
     origlen = len(oldelements)
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless=new')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument("window-size=1920,1080")
+
     chromedriver = "./chromedriver"
     driver = webdriver.Chrome(service = Service(chromedriver), options=chrome_options)
     # chromedriver = ChromeDriverManager().install()
