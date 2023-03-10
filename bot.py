be5497ca874ad1c212a9ec0f9aa7a2b3cf64d031
andrew-qian <andrewyukaiqian@gmail.com>
2023-03-09 20:27:45 -0600

    fuck you

--- bot.py
+++ bot.py
@@ -51,7 +51,6 @@ async def scrape():
         else:
             raise e
 
-
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="username"]')))
 
     username = driver.find_element(By.XPATH, '//*[@id="username"]')
